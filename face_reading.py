59643faf71e040650fe7e2f7ada04568567421cf
Mert Doğramacı <58953984+mertdogramaci@users.noreply.github.com>
2021-04-27 20:36:47 +0300

    Add files via upload

---
+++ face_reading.py
@@ -0,0 +1,39 @@
+import cv2
+import dlib
+
+# Load the detector
+detector = dlib.get_frontal_face_detector()
+
+# Load the predictor
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# read the image
+img = cv2.imread("Screenshot 2021-04-26 225708.png")
+
+# Convert image into grayscale
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+
+# Use detector to find landmarks
+faces = detector(gray)
+for face in faces:
+    x1 = face.left() # left point
+    y1 = face.top() # top point
+    x2 = face.right() # right point
+    y2 = face.bottom() # bottom point
+
+    # Create landmark object
+    landmarks = predictor(image=gray, box=face)
+
+    # Loop through all the points
+    for n in range(0, 68):
+        x = landmarks.part(n).x
+        y = landmarks.part(n).y
+
+        # Draw a circle
+        cv2.circle(img=img, center=(x, y), radius=2, color=(0, 255, 0), thickness=-1)
+
+# show the image
+cv2.imshow(winname="Face", mat=img)
+
+# Delay between every fram
+cv2.waitKey(delay=0)

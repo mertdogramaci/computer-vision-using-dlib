590c7c2078a72e1ffdc16da19ac1c00977b46f23
Mert Doğramacı <58953984+mertdogramaci@users.noreply.github.com>
2021-05-06 21:45:57 +0300

    Update face_reading_from_file.py

--- face_reading_from_file.py
+++ face_reading_from_file.py
@@ -29,7 +29,7 @@ def extract_images(file_path):
 
 def facial_landmarks(output_path):
     detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    predictor = dlib.shape_predictor("-pretrained landmark detector path-")
     img = cv2.imread(output_path)
     gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
     faces = detector(gray)
